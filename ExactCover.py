7c03c80209b551c0a9644b116a4b804ee71ae9de
Micah Brown <micahgbrown@gmail.com>
2021-08-18 20:30:27 +0100

    Added primary file

---
+++ ExactCover.py
@@ -0,0 +1,85 @@
+# Micah Brown 2021
+from numpy.core.fromnumeric import nonzero
+import numpy as np
+import collections
+
+def removeEleSet(cover, arrRemainingEle, arrRemainingSets, setIdx):
+    #Performs the deletion part of Algorithm X which involves deleting all
+    #elements that are in a set from our list as well as any set that contains these elements
+
+    corEleWith1 = np.nonzero( cover[arrRemainingEle, setIdx] )[0] #for the given setIdx (column) finds all the elements that are in that set
+    for ele in corEleWith1: #for each element that is in the set
+        corSetWith1 = np.nonzero( cover[arrRemainingEle[ele],arrRemainingSets])[0] #find all the sets (columns) that contain this element
+        arrRemainingSets = np.delete(arrRemainingSets, corSetWith1) #And delete them
+    arrRemainingEle = np.delete(arrRemainingEle, corEleWith1) #Then remove all the elements (rows) that are in the set we chose
+
+    return [arrRemainingEle, arrRemainingSets] #return the remaining Elements and Sets in an vector array
+
+def findMinIdxAndEle(cover, arrRemainingEle, arrRemainingSets):
+    #Count all the ones and find the first element with the lowest number of ones
+    count1s = np.count_nonzero( cover[arrRemainingEle,:][:, arrRemainingSets] , axis = 1 )
+    #take the index of this element
+    min1sEleIdx = arrRemainingEle[np.argmin(count1s)]
+
+    #Give a list of all the elements with this index
+    setsWithEle = arrRemainingSets[np.nonzero(cover[min1sEleIdx,arrRemainingSets])[0]]
+
+    return setsWithEle
+
+def removeReq(cover, arrRemainingEle,arrRemainingSets, reqlist):
+
+    for setIdx in reqlist:
+        [arrRemainingEle, arrRemainingSets] = removeEleSet(cover, arrRemainingEle, arrRemainingSets, setIdx)
+        _ = findMinIdxAndEle(cover, arrRemainingEle, arrRemainingSets)
+    
+    return [arrRemainingEle, arrRemainingSets]
+
+
+def algorithmX(cover, reqLst = []):
+    numberOfEle, numberOfSets = cover.shape
+
+    treeLoc = collections.deque([0]) #Will function as a stack with information on the order in the tree one is
+
+    arrRemainingEle = np.arange(numberOfEle)
+    arrRemainingSets = np.arange(numberOfSets)
+
+    [arrRemainingEle, arrRemainingSets] = removeReq(cover, arrRemainingEle, arrRemainingSets, reqLst)
+
+    treeArrs = [[arrRemainingEle,arrRemainingSets]] #Holds the remeining Elements and remaining Sets for each level of the tree, will work like a stack    
+    
+    treeSetsWithEle = collections.deque([]) #finds the index of the first row (element) with the lowest number of 1s
+    setsWithEle = findMinIdxAndEle(cover, arrRemainingEle, arrRemainingSets)
+    treeSetsWithEle.append(setsWithEle)
+
+    finalListOfSets = collections.deque([])
+    
+    #if there are no more elements, so the sets cover all the elements, return the list of sets
+    if treeArrs[-1][0].size == 0:
+        return list(finalListOfSets)
+    
+    while 1:        
+
+        #if the minimum number of 1s is 0 the problem isn't solved and we must go back up the tree
+        if setsWithEle.size == 0:
+
+            #goes through the tree, if all branches have been looked at it goes back up the tree until it can find a new branch
+            for i in range(len(treeLoc)):
+                if treeLoc[-1] < len(treeSetsWithEle[-1]) -1:
+                    treeLoc[-1] = treeLoc[-1] +1
+                    break
+                treeLoc.pop()
+                treeSetsWithEle.pop()
+                treeArrs.pop()
+                finalListOfSets.pop()
+            
+            setsWithEle = findMinIdxAndEle(cover, treeArrs[-1][0], treeArrs[-1][1])
+
+        else: #if the minimum number of ones is not 0 continue onto the next level of Tree
+            setIdx = treeSetsWithEle[-1][ treeLoc[-1] ] #Takes the matrix index of the set that we decided is in the solution
+            finalListOfSets.append(setIdx)
+            treeArrs.append( removeEleSet(cover, treeArrs[-1][0], treeArrs[-1][1], setIdx) )
+            if treeArrs[-1][0].size == 0:
+                return list(finalListOfSets)
+            setsWithEle = findMinIdxAndEle(cover, treeArrs[-1][0], treeArrs[-1][1])
+            treeSetsWithEle.append(setsWithEle)
+            treeLoc.append(0)
